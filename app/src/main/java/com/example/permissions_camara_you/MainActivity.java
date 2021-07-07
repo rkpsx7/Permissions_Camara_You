00a0c254156ea7c26a4aab3a469e432047dbcfcc
rkpsx7 <rkpsx7@gmail.com>
2021-07-07 13:33:10 +0530

    done

--- app/src/main/java/com/example/permissions_camara_you/MainActivity.java
+++ app/src/main/java/com/example/permissions_camara_you/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.permissions_camara_you;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int Req_Code = 0;
+    private Button mBtnCam_Permission;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnCam_Permission = findViewById(R.id.btnCamara);
+
+        mBtnCam_Permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] permissions = {Manifest.permission.CAMERA};
+                ActivityCompat.requestPermissions(MainActivity.this,permissions,Req_Code);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED)
+            showToast("Camara Permission Granted");
+        else if(grantResults[0]==PackageManager.PERMISSION_DENIED) {
+            showToast("Camara Permission Denied");
+            String[] permissions1 = {Manifest.permission.CAMERA};
+            ActivityCompat.requestPermissions(MainActivity.this, permissions1, Req_Code);
+        }
+    }
+
+    public void showToast(String str){
+        Toast.makeText(this,str,Toast.LENGTH_SHORT).show();
     }
 }
